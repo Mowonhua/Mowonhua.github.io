87fba214b9702ee2fd69a5f2940aae9df2c5e9a0
花月雪真君 <h1812088227@163.com>
2025-12-29 14:38:32 +0800

    fit for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://moblog.com',
+	site: 'https://mowonhua.github.io',
 	integrations: [mdx(), sitemap()],
 });
